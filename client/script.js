f2c3b48224f9649fce2cf8fc0a1300481068f4aa
El Agha Suhaib <129865576+Saturoi@users.noreply.github.com>
2025-11-21 04:28:42 +0200

    Change WebSocket URL to secure connection

--- client/script.js
+++ client/script.js
@@ -1,7 +1,7 @@
 // =============================
 // CONFIG
 // =============================
-const SERVER_URL = "https://barraalsalfaonline.onrender.com/";  // أثناء التجربة المحلية
+const SERVER_URL = "wss://barraalsalfaonline.onrender.com";  // أثناء التجربة المحلية
 
 // =============================
 // المتغيرات العامة
@@ -123,6 +123,7 @@ restartBtn.onclick = () => {
 // =============================
 // تشغيل الاتصال
 connectToServer();
+
 
 
 
